c64b54cc2d915916ddf2e87d4f1ad2a7b7e3faf8
otsukaresamaja <christophersugai@gmail.com>
2021-03-14 18:58:31 -0500

    sra download files

---
+++ R/sra_download.R
@@ -0,0 +1,12 @@
+#' Intakes a path for an SRR_Acc_List.txt file from SRA and downloads the files into a destination directory
+#' @export
+
+sra_download = function(SRR_Acc_List, destination_directory){
+  top_url="https://trace.ncbi.nlm.nih.gov/Traces/sra?run="
+  # Read and create SRR list from SRR_Acc_List.txt
+  accessions=as.vector(read.table(SRR_Acc_List))[[1]]
+  for (i in 1:length(accessions)){
+    SRR_url=paste0(top_url, accessions[i])
+    download.file(url=SRR_url, destfile = paste(destination_directory, accessions[i], sep="/"))
+  }
+}
