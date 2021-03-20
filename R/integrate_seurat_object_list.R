a0e2b19134c25b8fd3b2c0ed3ccda598908c7f56
otsukaresamaja <christophersugai@gmail.com>
2021-03-19 21:58:24 -0500

    more build bugs

--- R/integrate_seurat_object_list.R
+++ R/integrate_seurat_object_list.R
@@ -2,14 +2,14 @@
 #' @export
 
 integrate_seurat_object_list=function(seurat_object_list, npcs, dims){
-features=SelectIntegrationFeatures(object.list = seurat_object_list)
-anchors=FindIntegrationAnchors(object.list = seurat_object_list, anchor.features = features)
-integrated_seurat_object=IntegrateData(anchorset = anchors)
-DefaultAssay(integrated_seurat_object) <- "integrated"
-integrated_seurat_object=ScaleData(integrated_seurat_object, verbose = FALSE)
-integrated_seurat_object=RunPCA(integrated_seurat_object, npcs = npcs, verbose = FALSE)
-integrated_seurat_object=RunUMAP(integrated_seurat_object, reduction = "pca", dims = 1:dims)
-integrated_seurat_object=FindNeighbors(integrated_seurat_object, reduction = "pca", dims = 1:dims)
-integrated_seurat_object=FindClusters(integrated_seurat_object, resolution = 0.5)
+features=Seurat::SelectIntegrationFeatures(object.list = seurat_object_list)
+anchors=Seurat::FindIntegrationAnchors(object.list = seurat_object_list, anchor.features = features)
+integrated_seurat_object=Seurat::IntegrateData(anchorset = anchors)
+Seurat::DefaultAssay(integrated_seurat_object) <- "integrated"
+integrated_seurat_object=Seurat::ScaleData(integrated_seurat_object, verbose = FALSE)
+integrated_seurat_object=Seurat::RunPCA(integrated_seurat_object, npcs = npcs, verbose = FALSE)
+integrated_seurat_object=Seurat::RunUMAP(integrated_seurat_object, reduction = "pca", dims = 1:dims)
+integrated_seurat_object=Seurat::FindNeighbors(integrated_seurat_object, reduction = "pca", dims = 1:dims)
+integrated_seurat_object=Seurat::FindClusters(integrated_seurat_object, resolution = 0.5)
 return(integrated_seurat_object)
 }
