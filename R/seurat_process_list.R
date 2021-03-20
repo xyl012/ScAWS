c9fa595ea48e6a43fcc1a815e70961fce608d322
otsukaresamaja <christophersugai@gmail.com>
2021-03-19 21:54:05 -0500

    npcs bug

--- R/seurat_process_list.R
+++ R/seurat_process_list.R
@@ -1,10 +1,10 @@
 #' Intakes a list of seurat objects and outputs a processed list of seurat objects
 #' @export
 
-seurat_process_list=function(seurat_object_list){
+seurat_process_list=function(seurat_object_list, npcs, dims){
   seurat_processed_list=list()
   for (i in 1:length(seurat_object_list)){
-    seurat_processed_list[[i]]=seurat_process_object(seurat_object_list[[i]])
+    seurat_processed_list[[i]]=seurat_process_object(seurat_object_list[[i]], npcs=npcs, dims=dims)
   }
   return(seurat_processed_list)
 }
