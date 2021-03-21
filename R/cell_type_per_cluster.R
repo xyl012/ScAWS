fdfa4195bdc46e412347354503c5563711d98fba
otsukaresamaja <christophersugai@gmail.com>
2021-03-20 23:29:26 -0500

    celltype percents

---
+++ R/cell_type_per_cluster.R
@@ -0,0 +1,20 @@
+#' Get the count or percentage of cell type per cluster. Input is from seurat_process. output options are "count" for cell counts or "percent" as cell count percents of the cluster.
+#' @export
+cell_type_per_cluster=function(seurat_object, output="count"){
+  celltypes=unique(seurat_object$celltype)
+  clusters=sort(levels(seurat_object$seurat_clusters))
+  celltype_cluster_list=list()
+  for (i in 1:length(clusters)){
+    subset=subset(seurat_object, seurat_clusters==clusters[i])
+    subset_celltype_names=names(table(subset$celltype))
+    subset_celltype_counts=as.vector(table(subset$celltype))
+    if (output=="count"){
+    df=data.frame(row.names = subset_celltype_names, subset_celltype_counts)
+    celltype_cluster_list[[i]]=df
+    } else if(output=="percent") {
+    df=data.frame(row.names = subset_celltype_names, subset_celltype_counts/sum(subset_celltype_counts))
+    celltype_cluster_list[[i]]=df
+    }
+  }
+  return(celltype_cluster_list)
+}
