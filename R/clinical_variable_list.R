26463440b7696075f76ff0ed3282d13d97938bc1
otsukaresamaja <christophersugai@gmail.com>
2021-03-20 20:26:56 -0500

    typo

--- R/clinical_variable_list.R
+++ R/clinical_variable_list.R
@@ -2,7 +2,7 @@
 #' @export
 
 clinical_variable_list=function(clinical_variable){
-  clinical_variable_objet=list()
+  clinical_variable_object=list()
   clinical_variable_object[[1]]=clinical_variable #Data of the clinical variable
   clinical_variable_object[[2]]=variable_name_as_string(clinical_variable) #Name of the clinical variable as a string for graphing
   return(clinical_variable_object)
