06e6441309bc46bc41e44520abd113a9e57058dd
otsukaresamaja <christophersugai@gmail.com>
2021-03-21 19:40:52 -0500

    vignette partial1

--- R/cell_type_per_cluster.R
+++ R/cell_type_per_cluster.R
@@ -1,5 +1,6 @@
 #' Get the count or percentage of cell type per cluster. Input is from seurat_process. output options are "count" for cell counts or "percent" as cell count percents of the cluster.
 #' @export
+
 cell_type_per_cluster=function(seurat_object, output="count"){
   celltypes=unique(seurat_object$celltype)
   clusters=sort(levels(seurat_object$seurat_clusters))
@@ -9,12 +10,15 @@ cell_type_per_cluster=function(seurat_object, output="count"){
     subset_celltype_names=names(table(subset$celltype))
     subset_celltype_counts=as.vector(table(subset$celltype))
     if (output=="count"){
-    df=data.frame(row.names = subset_celltype_names, subset_celltype_counts)
-    celltype_cluster_list[[i]]=df
+      df=data.frame(celltype = subset_celltype_names, counts = subset_celltype_counts)
+      celltype_cluster_list[[i]]=df
     } else if(output=="percent") {
-    df=data.frame(row.names = subset_celltype_names, subset_celltype_counts/sum(subset_celltype_counts))
-    celltype_cluster_list[[i]]=df
+      df=data.frame(celltype = subset_celltype_names, percents = subset_celltype_counts/sum(subset_celltype_counts))
+      celltype_cluster_list[[i]]=df
     }
   }
-  return(celltype_cluster_list)
+  celltype_cluster_df=Reduce(function(d1, d2) merge(d1, d2, by = "celltype", all = TRUE),celltype_cluster_list)
+  colnames(celltype_cluster_df)=c("celltype", clusters)
+  celltype_cluster_df[is.na(celltype_cluster_df)]<-0
+  return(celltype_cluster_df)
 }
---
+++ R/plot_corrplot_celltype.R
@@ -0,0 +1,9 @@
+#' Plots a pie chart of percentage cell types in each cell cluster. Input is a seurat object
+#' @export
+
+plot_corrplot_celltype=function(seurat_object){
+celltype_df=cell_type_per_cluster(seurat_object, output="percent")
+ncelltype_df=celltype_df[,-1]; clusters=colnames(ncelltype_df); ncelltype_df=as.matrix(ncelltype_df); rownames(ncelltype_df)=celltype_df$celltype; colnames(ncelltype_df)=clusters
+plot=recordPlot(corrplot::corrplot(ncelltype_df, method = "pie", cl.pos = "n", col = bi[1], cl.lim = c(0, 1), tl.col = "black"))
+return(plot)
+}
