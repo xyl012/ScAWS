209cfbb0ae4bf76ba19c56ca4420448d6cbfcde6
otsukaresamaja <christophersugai@gmail.com>
2021-03-23 17:27:53 -0500

    vignette

--- R/plot_corrplot_celltype.R
+++ R/plot_corrplot_celltype.R
@@ -7,7 +7,7 @@ plot_corrplot_celltype=function(seurat_object, col){
   ncelltype_df=ncelltype_df[ , as.character(0:(ncol(ncelltype_df)-1))]
   rownames(ncelltype_df)=celltype_df$celltype; colnames(ncelltype_df)=colnames(celltype_df[,-1])
   ncelltype_df=as.matrix(ncelltype_df)
-  corrplot::corrplot(ncelltype_df, ccl.lim = c(0, 1), tl.col = "black", col = col, cl.lim = c(0,1))
+  corrplot::corrplot(ncelltype_df, ccl.lim = c(0, 1), tl.col = "black", col = col, cl.lim = c(0,1), method="circle")
   celltype_df=cell_type_per_cluster(seurat_object, output="count")
   ncelltype_df=celltype_df[,-1]
   ncelltype_df=ncelltype_df[ , as.character(0:(ncol(ncelltype_df)-1))]
